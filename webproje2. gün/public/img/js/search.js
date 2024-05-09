ef610e94a531c8208f4344375e73655cb1f3c653
Kadirdyn4 <90631785+Kadirduyan@users.noreply.github.com>
2024-05-09 19:21:36 +0300

    web projesi 2. gün
    bugün genelde topbar yapısnı oluşturdum hakkımda memleketim cv kısımlarını ekledim

---
+++ webproje2. gün/public/img/js/search.js
@@ -0,0 +1,8 @@
+const searchIcon = document.getElementById("searchIconé")
+const searchEngine = document.getElementById("searchIconé")
+
+
+
+searchIcon.addEventListener('click',()=>{
+    searchEngine.classList.toggle('active')
+})
